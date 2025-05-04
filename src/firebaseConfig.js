07472543294c169dd57da217cf033b854b2ba8e2
Rae Magpantay <raealjon2003@gmail.com>
2025-05-05 05:05:51 +0800

    firebase api

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "https://planetary-deep-sea-survival-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDkN68KeZw478TuXiS0g3KKMYHQN3zFmXw", // Actual API key
+  authDomain: "planetary-deep-sea-survival.firebaseapp.com", // Actual Auth Domain
+  databaseURL: "https://planetary-deep-sea-survival-default-rtdb.asia-southeast1.firebasedatabase.app", // Actual Database URL
+  projectId: "planetary-deep-sea-survival", // Actual Project ID
+  storageBucket: "planetary-deep-sea-survival.firebasestorage.app", // Actual Storage Bucket
+  messagingSenderId: "19888976863", // Actual Messaging Sender ID
+  appId: "1:19888976863:web:9c63ce282e2fd4f7f3ca6a", // Actual App ID
 };
 
 const app = initializeApp(firebaseConfig);
